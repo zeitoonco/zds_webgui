7ff2630ecd378b269ff9e1784149344b94a33f08
akane0151 <akane0151@yahoo.com>
2016-08-30 11:22:01 +0430

    تغییرات در ماژول ها

--- app/pages/user-mng/user/user.module.js
+++ app/pages/user-mng/user/user.module.js
@@ -4,9 +4,38 @@
 (function () {
     'use strict';
 
-    angular.module('ZDSGUI.pages.user-mng.user', [])
+    var user = angular.module('ZDSGUI.pages.user-mng.user', [])
         .config(routeConfig);
+    user.controller('useraction',function ($scope,zdsSocket) {
 
+        $scope.getlistuser = function () {
+           // $scope.LoginDisabled = true;
+
+            var msg = {
+                type: "call",
+                node: "userman.listUsers",
+                id: "1234568"
+            };
+            if (zdsSocket.status() == 1) {
+                zdsSocket.send(msg, function (data) {
+                    if (data["data"]["success"] == "true") {
+                        $scope.result = "True";
+
+                    } else {
+                        toastr.error('!', 'خطا!');
+                        //$scope.LoginDisabled = false;
+
+                    }
+                });
+            } else {
+                toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
+
+            }
+            //console.log("Hello! " + $scope.username)
+
+        }
+
+    });
 
     function routeConfig($stateProvider) {
         $stateProvider
