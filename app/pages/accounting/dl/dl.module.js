89b20f59d2e28be5dcbea20a8c39289c5d561599
Pedram <carp3co@gmail.com>
2016-09-26 17:02:14 +0330

    Merge branch 'master' of https://github.com/zeitoonco/zds_webgui
    # Conflicts:
    #	app/main.rtl.css

--- app/pages/accounting/dl/dl.module.js
+++ app/pages/accounting/dl/dl.module.js
@@ -4,15 +4,15 @@
 (function () {
     var dlid;
     'use strict';
-    var dl = angular.module('ZDSGUI.pages.accounting.dl',['ZDSGUI.boolean'])
+    var dl = angular.module('ZDSGUI.pages.accounting.dl', ['ZDSGUI.boolean'])
         .config(routeConfig);
-    dl.controller('newdl',function ($scope,zdsSocket,toastr) {
+    dl.controller('newdl', function ($scope, zdsSocket, toastr) {
 
         $scope.adddl = function () {
             var msg = {
                 type: "call",
                 node: "AccountingRelay.newDl",
-                data:{userid: uid,code: $scope.code,title: $scope.title,title2: $scope.title,type: 1,isactive: 1}
+                data: {userid: uid, code: $scope.code, title: $scope.title, title2: $scope.title, type: 1, isactive: 1}
             };
             if (zdsSocket.status() == 1) {
                 console.log(JSON.stringify(msg));
@@ -30,34 +30,38 @@
             }
         }
     });
-    dl.controller('editdl',function ($scope,zdsSocket,toastr) {
+    dl.controller('editdl', function ($scope, zdsSocket, toastr) {
 
         $scope.modifydl = function () {
             var msg = {
                 type: "call",
                 node: "AccountingRelay.modifyDL",
-                data:{userid: uid,id: dlid,code: $scope.code,title: $scope.title,title2: $scope.title,type:1,isactive: $scope.en}
+                data: {
+                    userid: uid,
+                    id: dlid,
+                    code: $scope.code,
+                    title: $scope.title,
+                    title2: $scope.title,
+                    type: 1,
+                    isactive: $scope.en
+                }
             };
-            if (zdsSocket.status() == 1) {
-                console.log(JSON.stringify(msg));
-                zdsSocket.send(msg, function (data) {
-                    if (data["success"] == true) {
-                        toastr.success('اطلاعات اعمال شد!');
+            console.log(JSON.stringify(msg));
+            zdsSocket.send(msg, function (data) {
+                if (data["success"] == true) {
+                    toastr.success('اطلاعات اعمال شد!');
 
-                    } else {
-                        toastr.error('!', 'خطا!');
-                    }
-                });
-            } else {
-                toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
+                } else {
+                    toastr.error('!', 'خطا!');
+                }
+            });
 
-            }
         }
     });
 
 
-    dl.controller('dl',function ($scope,zdsSocket,toastr,$uibModal) {
-        $scope.openmodal = function (page, size, id,code,title,en) {
+    dl.controller('dl', function ($scope, zdsSocket, toastr, $uibModal) {
+        $scope.openmodal = function (page, size, id, code, title, en) {
             dlid = id;
             $scope.code = code;
             $scope.title = title;
@@ -80,45 +84,36 @@
             var msg = {
                 type: "call",
                 node: "AccountingRelay.query",
-                data: {'table': 'DL','where': [['del', '=', '0']]}
+                data: {'table': 'DL', 'where': [['del', '=', '0']]}
             };
-            if (zdsSocket.status() == 1) {
                 console.log(JSON.stringify(msg));
                 zdsSocket.send(msg, function (data) {
                     if (data["success"] == true) {
-                       $scope.myData = data.data.result.rows;
+                        $scope.myData = data.data.result.rows;
                         toastr.success('اطلاعات با موفقیت دریافت شد!');
                     } else {
                         toastr.error('!', 'خطا!');
                     }
                 });
-            } else {
-                toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
-
-            }
 
         }
         $scope.doremove = function (id) {
-            var alert = confirm("آیا از حذف این حساب مطمئن هستید؟");
-            if (alert == true){
-            var msg = {
-                type: "call",
-                node: "AccountingRelay.removeDL",
-                data: {userid: uid,id: id}
-            };
-            if (zdsSocket.status() == 1) {
-                console.log(JSON.stringify(msg));
-                zdsSocket.send(msg, function (data) {
-                    if (data["success"] == true) {
-                        toastr.success('تفضیلی با موفقیت حذف شد!');
-                    } else {
-                        toastr.error('!', 'خطا!');
-                    }
-                });
-            } else {
-                toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
+            var alert = confirm("آیا از حذف این تفضیلی مطمئن هستید؟");
+            if (alert == true) {
+                var msg = {
+                    type: "call",
+                    node: "AccountingRelay.removeDL",
+                    data: {userid: uid, id: id}
+                };
+                    console.log(JSON.stringify(msg));
+                    zdsSocket.send(msg, function (data) {
+                        if (data["success"] == true) {
+                            toastr.success('تفضیلی با موفقیت حذف شد!');
+                        } else {
+                            toastr.error('!', 'خطا!');
+                        }
+                    });
 
-            }
             } else {
 
             }
@@ -132,7 +127,7 @@
             .state('accounting.dl', {
                 url: '/dl',
                 templateUrl: 'app/pages/accounting/dl/dl.html',
-                title: 'تفضیلی',
+                title: 'تفضیلی سایر',
                 sidebarMeta: {
                     order: 900
                 }
