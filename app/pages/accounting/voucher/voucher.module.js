130efd30f61a5ca948f20b4f5f158d95bc7acf01
akane0151 <akane0151@yahoo.com>
2016-09-28 15:41:38 +0330

    سند مالی

--- app/pages/accounting/voucher/voucher.module.js
+++ app/pages/accounting/voucher/voucher.module.js
@@ -6,6 +6,7 @@
     'use strict';
     var voucher = angular.module('ZDSGUI.pages.accounting.voucher', ['ngJalaliFlatDatepicker'])
         .config(routeConfig);
+
     voucher.controller('newvoucher', function ($scope, zdsSocket, toastr) {
 
         $scope.dlEnabled = [true];
@@ -20,7 +21,7 @@
                 debit: '0',
                 credit: '0',
                 trackingnumber: '0',
-                trackingdate: '0000-00-00',
+                trackingdate: '2000-11-11',
                 description: '--'
             });
 
@@ -75,14 +76,9 @@
                     toastr.error('!', 'خطا!');
                 }
             });
-
         };
-        /*$scope.dlEnabledFunction = function (row) {
-            console.log("yay" + row);
-            return (row in $scope.dlEnabled);
-        };*/
-        $scope.checkdl = function (d, index) {
 
+        $scope.checkdl = function (d, index) {
             $scope.dlIndexId[d] = index;
             var msg = {
                 type: "call",
@@ -91,7 +87,6 @@
                     'table': 'Account',
                     'columns': ['hasdl', 'accountid'],
                     'where': [['accountid', '=', d]]
-
                 }
             };
             zdsSocket.send(msg, function (data) {
@@ -99,8 +94,6 @@
                     $scope.hasdl = data.data.result.rows[0][0];
 
                     if ($scope.hasdl == 't') {
-                          //$scope.dlEnabled[$scope.dlIndexId[parseInt(data.data.result.rows[0][1])]] = true;
-
                         var c = 'dl' + $scope.dlIndexId[parseInt(data.data.result.rows[0][1])];
                         document.getElementById(c).className = "";
                     } else {
@@ -114,15 +107,6 @@
             });
 
         };
-        $scope.check = function (id) {
-            alert(id);
-            if (id[1] == 'f') {
-                return true;
-            } else if (id[1] == 't') {
-                return false;
-            }
-        };
-
         $scope.getaccountid = function () {
             var msg = {
                 type: "call",
@@ -145,13 +129,34 @@
                 });
             } else {
                 toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
-
             }
-
-
         }
+        $scope.getdlid = function () {
+            var msg = {
+                type: "call",
+                node: "AccountingRelay.query",
+                data: {
+                    'table': 'DL',
+                    'columns': ['dlid' ,'code' , 'title2'],
+                    'where': [['del', '=', '0', 'AND'], ['isactive', '=', '1']]
 
+                }
+            };
+            if (zdsSocket.status() == 1) {
+                zdsSocket.send(msg, function (data) {
+                    if (data["success"] == true) {
+                        $scope.dlid = data.data.result.rows;
+                        console.log(JSON.stringify($scope.acid));
+                    } else {
+                        toastr.error('!', 'خطا!');
+                    }
+                });
+            } else {
+                toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
+            }
+        }
         $scope.getaccountid();
+        $scope.getdlid();
 
         $scope.datepickerConfig = {
             dateFormat: 'jYYYY/jMM/jDD',
@@ -162,23 +167,169 @@
     });
 
 
+
+
     voucher.controller('editvoucher', function ($scope, zdsSocket, toastr) {
-        $scope.get = function (id) {
+        $scope.dlEnabled = [true];
+        $scope.dlIndexId = [];
+        $scope.items = []; //voucher items
+
+        //add empty object as row in items
+        $scope.additem = function () {
+            $scope.items.push([
+                '0',
+                '0',
+                '--',
+                '0',
+                '0',
+                '0',
+                '2000-11-11'
+            ]);
+
+        };
+
+        //print log of items content as object for test
+        $scope.itemlog = function () {
+            console.log(JSON.stringify($scope.items));
+        };
+        //remove a row in items by index
+        $scope.removeitem = function (index) {
+            $scope.items.splice(index, 1);
+            $scope.difference();
+        };
+
+        $scope.difference = function () {
+            var sd = 0, sc = 0;
+            for (var i in $scope.items) {
+                sd = sd + parseInt($scope.items[i].debit);
+                sc = sc + parseInt($scope.items[i].credit);
+            }
+            $scope.d = parseInt(sd - sc);
+            if ($scope.d > 0 || $scope.d < 0){
+                $scope.dcolor = 'red';
+            } else {
+                $scope.dcolor = 'grey';
+            }
+        };
+        $scope.checkdl = function (d, index) {
+            $scope.dlIndexId[d] = index;
             var msg = {
                 type: "call",
                 node: "AccountingRelay.query",
-                data: {'table': 'voucher', 'where': [['id', '=', id]]}
+                data: {
+                    'table': 'Account',
+                    'columns': ['hasdl', 'accountid'],
+                    'where': [['accountid', '=', d]]
+                }
+            };
+            zdsSocket.send(msg, function (data) {
+                if (data["success"] == true) {
+                    $scope.hasdl = data.data.result.rows[0][0];
+
+                    if ($scope.hasdl == 't') {
+                        var c = 'dl' + $scope.dlIndexId[parseInt(data.data.result.rows[0][1])];
+                        document.getElementById(c).className = "";
+                    } else {
+                        var c = 'dl' + $scope.dlIndexId[parseInt(data.data.result.rows[0][1])];
+                        document.getElementById(c).className = "dldisabled";
+                    }
+                    console.log(JSON.stringify($scope.acid));
+                } else {
+                    toastr.error('!', 'خطا!');
+                }
+            });
+
+        };
+        $scope.getaccountid = function () {
+            var msg = {
+                type: "call",
+                node: "AccountingRelay.query",
+                data: {
+                    'table': 'Account',
+                    'columns': ['accountid', 'hasdl', 'title2'],
+                    'where': [['type', '=', '3', 'AND'], ['del', '=', '0']]
+
+                }
+            };
+            if (zdsSocket.status() == 1) {
+                zdsSocket.send(msg, function (data) {
+                    if (data["success"] == true) {
+                        $scope.acid = data.data.result.rows;
+                        console.log(JSON.stringify($scope.acid));
+                    } else {
+                        toastr.error('!', 'خطا!');
+                    }
+                });
+            } else {
+                toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
+            }
+        }
+        $scope.getdlid = function () {
+            var msg = {
+                type: "call",
+                node: "AccountingRelay.query",
+                data: {
+                    'table': 'DL',
+                    'columns': ['dlid' ,'code' , 'title2'],
+                    'where': [['del', '=', '0', 'AND'], ['isactive', '=', '1']]
+
+                }
+            };
+            if (zdsSocket.status() == 1) {
+                zdsSocket.send(msg, function (data) {
+                    if (data["success"] == true) {
+                        $scope.dlid = data.data.result.rows;
+                        console.log(JSON.stringify($scope.acid));
+                    } else {
+                        toastr.error('!', 'خطا!');
+                    }
+                });
+            } else {
+                toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
+            }
+        }
+        $scope.getaccountid();
+        $scope.getdlid();
+        $scope.getinfo = function () {
+            var msg = {
+                type: "call",
+                node: "AccountingRelay.query",
+                data: {'table': 'voucher', 'where': [['voucherid', '=', $scope.vid]]}
             };
             if (zdsSocket.status() == 1) {
                 console.log(JSON.stringify(msg));
                 zdsSocket.send(msg, function (data) {
                     if (data["success"] == true) {
-                        $scope.myData = data.data.result.rows;
-                        $scope.accountid = $scope.myData[1];
-                        $scope.referencenumber = $scope.myData[2];
-                        $scope.accountid = $scope.myData[1];
-                        $scope.accountid = $scope.myData[1];
-                        $scope.accountid = $scope.myData[1];
+                        $scope.myData = data.data.result.rows[0];
+                        $scope.number = parseInt($scope.myData[1]);
+                        $scope.refnumber = parseInt($scope.myData[3]);
+                        $scope.date = moment($scope.myData[2]).format('jYYYY/jMM/jDD');
+                        $scope.dnumber = $scope.myData[10];
+                        $scope.dec = $scope.myData[8];
+                        $scope.type = $scope.myData[6];
+                        $scope.type = $scope.myData[6];
+                        $scope.type = $scope.myData[6];
+                        toastr.success('اطلاعات با موفقیت دریافت شد!');
+                    } else {
+                        toastr.error('!', 'خطا!');
+                    }
+                });
+            } else {
+                toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
+
+            }
+            var msg = {
+                type: "call",
+                node: "AccountingRelay.query",
+                data: {'table': 'voucheritem',
+                    'columns': ['accountid', 'dlid','description', 'debit::INT8', 'credit::INT8','trackingnumber','trackingdate' ],
+                    'where': [['voucherid', '=', $scope.vid]]}
+            };
+            if (zdsSocket.status() == 1) {
+                console.log(JSON.stringify(msg));
+                zdsSocket.send(msg, function (data) {
+                    if (data["success"] == true) {
+                        $scope.items = data.data.result.rows;
                         toastr.success('اطلاعات با موفقیت دریافت شد!');
                     } else {
                         toastr.error('!', 'خطا!');
@@ -191,37 +342,40 @@
         }
 
 
-        $scope.modifycategory = function () {
+        $scope.modifyvoucher = function () {
             var msg = {
                 type: "call",
-                node: "AccountingRelay.modifyVoucher",
-                data: {userid: uid, id: cid, title: $scope.title, type: $scope.type.value}
+                node: "AccountingRelay.modifyNewVocher",
+                data: {
+                    userid: uid,
+                    id: $scope.vid,
+                    number: $scope.number,
+                    date: $scope.fvdate,
+                    referencenumber: $scope.refnumber,
+                    secondarynumber: 0,
+                    state: 1,
+                    type: 1,
+                    description: $scope.dec,
+                    items: $scope.items
+                }
             };
-            if (zdsSocket.status() == 1) {
-                console.log(JSON.stringify(msg));
-                zdsSocket.send(msg, function (data) {
-                    if (data["success"] == true) {
-                        toastr.success('اطلاعات اعمال شد!');
+            console.log(JSON.stringify(msg));
+            zdsSocket.send(msg, function (data) {
+                if (data["success"] == true) {
+                    toastr.success('طبقه ی جدید اضافه شد!');
 
-                    } else {
-                        toastr.error('!', 'خطا!');
-                    }
-                });
-            } else {
-                toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
-
-            }
+                } else {
+                    toastr.error('!', 'خطا!');
+                }
+            });
         }
-
-
+        $scope.getinfo();
     });
 
 
     voucher.controller('voucher', function ($scope, zdsSocket, toastr, $uibModal) {
-        $scope.openmodal = function (page, size, id, type, title) {
-            cid = id;
-            $scope.type = type;
-            $scope.title = title;
+        $scope.openmodal = function (page, size, id) {
+            $scope.vid = id;
             $uibModal.open({
                 animation: true,
                 templateUrl: page,
@@ -259,13 +413,18 @@
             }
 
         }
+
+        $scope.getvoucher();
+    });
+
+    voucher.controller('removevoucher',function ($scope,zdsSocket,$uibModal,toastr) {
+
+
         $scope.doremove = function (id) {
-            var alert = confirm("آیا از حذف این طبقه مطمئن هستید؟");
-            if (alert == true) {
                 var msg = {
                     type: "call",
                     node: "AccountingRelay.removeVoucher",
-                    data: {userid: uid, id: id}
+                    data: {userid: uid, id: $scope.vid}
                 };
                 if (zdsSocket.status() == 1) {
                     console.log(JSON.stringify(msg));
@@ -280,14 +439,8 @@
                     toastr.error('اتصال با وبسوکت برقرار نیست!!', 'خطا!');
 
                 }
-            } else {
-
-            }
-
         }
-        $scope.getvoucher();
     });
-
 
     voucher.filter('jalaliDate', function () {
         return function (inputDate, format) {
