288b575b3273b35f4a6392580c86f6d65d50a253
akane0151 <akane0151@yahoo.com>
2016-10-18 14:48:28 +0330

    تفضیلی

--- app/pages/user-mng/group-mng/group-mng.module.js
+++ app/pages/user-mng/group-mng/group-mng.module.js
@@ -6,7 +6,7 @@
 
     var groupmng = angular.module('ZDSGUI.pages.user-mng.group-mng', [])
         .config(routeConfig);
-    groupmng.controller('group-mng',function ($scope,zdsSocket,$uibModal) {
+    groupmng.controller('group-mng',function ($scope,zdsSocket,toastr,$uibModal) {
         $scope.getgroups = function () {
             var msg = {
                 type: "call",
@@ -21,10 +21,11 @@
                 }
             });
         }
-        $scope.openmodal = function (page, size, id,t,d) {
-            $scope.id = id;
+        $scope.openmodal = function (page, size, id,t,d,pid) {
+            $scope.gid = id;
             $scope.title = t;
             $scope.dec = d;
+            $scope.pid = pid;
             $uibModal.open({
                 animation: true,
                 templateUrl: page,
@@ -42,7 +43,7 @@
 
     });
 
-    groupmng.controller('newgroup',function ($scope,zdsSocket,$uibModal) {
+    groupmng.controller('newgroup',function ($scope,zdsSocket,$uibModal,toastr) {
         $scope.$on('modal.closing', function (event, reason, closed) {
             $scope.getgroups();
         });
@@ -58,7 +59,7 @@
             console.log(JSON.stringify(msg));
             zdsSocket.send(msg, function (data) {
                 console.log(JSON.stringify(data));
-                if (data["data"]["result"] == true) {
+                if (data["success"] == true) {
                     toastr.success('گروه اضافه شد!');
                 } else {
                     toastr.error('!', 'خطا!');
@@ -85,7 +86,7 @@
         $scope.getgroup();
     });
 
-    groupmng.controller('editgroup',function ($scope,zdsSocket,$uibModal) {
+    groupmng.controller('editgroup',function ($scope,zdsSocket,$uibModal,toastr) {
         $scope.$on('modal.closing', function (event, reason, closed) {
             $scope.getgroups();
         });
@@ -94,12 +95,50 @@
             var msg = {
                 type: "call",
                 node: "userman.updateUsergroup",
-                data: {GroupID:$scope.gid,title: $scope.title,parentID: $scope.pid,description: $scope.dec}
+                data: {userGroupID:$scope.gid,title: $scope.title,parentID: $scope.pid,description: $scope.dec}
             };
             console.log(JSON.stringify(msg));
             zdsSocket.send(msg, function (data) {
-                if (data["data"]["result"] == true) {
+                if (data["success"] == true) {
                     toastr.success('اطلاعات با موفقیت اعمال شد!');
+                } else {
+                    toastr.error('!', 'خطا!');
+                }
+            });
+        }
+        $scope.getgroup = function (){
+            var msg = {
+                type: "call",
+                node: "userman.listUsergroups"
+            };
+            zdsSocket.send(msg, function (data) {
+                console.log(JSON.stringify(data));
+                if (data["success"] == true) {
+                    $scope.grouplist = data['data']['userGroupsList'];
+                    $scope.grouplist.push({"usergroupID":-1,"title":"سرشاخه","parentID":-1,"description":"test1"});
+                } else {
+                    toastr.error('!', 'خطا!');
+                }
+            });
+        }
+
+        $scope.getgroup();
+    });
+    groupmng.controller('removegroup',function ($scope,zdsSocket,toastr,$uibModal) {
+        $scope.$on('modal.closing', function (event, reason, closed) {
+            $scope.getgroups();
+        });
+
+        $scope.doremove = function () {
+            var msg = {
+                type: "call",
+                node: "userman.removeUsergroup",
+                data: {value:$scope.gid}
+            };
+            console.log(JSON.stringify(msg));
+            zdsSocket.send(msg, function (data) {
+                if (data["success"] == true) {
+                    toastr.success('گروه مورد نظر حذف شد!');
                 } else {
                     toastr.error('!', 'خطا!');
                 }
@@ -114,7 +153,7 @@
             .state('user-mng.group-mng', {
                 url: '/group-mng',
                 templateUrl : 'app/pages/user-mng/group-mng/group-mng.html',
-                title: 'دسته بندی کاربران',
+                title: 'گروه ها',
                 permission: 'userman.groupmng',
                 sidebarMeta: {
                     icon: 'ion-person-stalker',
