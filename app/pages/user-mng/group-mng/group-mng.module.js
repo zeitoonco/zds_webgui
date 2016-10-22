5f82f3eb3dc3d99a06a7a5c77dc9d781fab1c2e9
akane0151 <akane0151@yahoo.com>
2016-10-22 15:11:03 +0330

    تفضیلی

--- app/pages/user-mng/group-mng/group-mng.module.js
+++ app/pages/user-mng/group-mng/group-mng.module.js
@@ -38,9 +38,7 @@
                 }
             });
         }
-
         $scope.getgroups();
-
     });
 
     groupmng.controller('newgroup',function ($scope,zdsSocket,$uibModal,toastr) {
@@ -146,7 +144,54 @@
         }
     });
 
-    groupmng.controller('editgroupperm',function ($scope,zdsSocket,$uibModal,toastr) {
+    groupmng.controller('editgroupperm',function ($scope,zdsSocket,$uibModal,toastr,$TreeDnDConvert) {
+        var tree;
+        $scope.tree_data = {};
+        $scope.my_tree = tree = {};
+
+        $scope.my_tree.addFunction = function (node) {
+            console.log(node);
+            alert('Function added in Controller "App.js"');
+        };
+
+        $scope.expanding_property = {
+            /*template: "<td>OK All</td>",*/
+            field: 'Name',
+            titleClass:  'text-center',
+            cellClass:   'v-middle',
+            displayName: 'Name'
+        };
+        $scope.col_defs = [
+            {
+                field: 'Description'
+            }, {
+                field:        'Description',
+                titleStyle:   {
+                    'width': '80pt'
+                },
+                titleClass:   'text-center',
+                cellClass:    'v-middle text-center',
+                displayName:  'Description',
+                cellTemplate: '<i class="fa {{ !node.Description ? \'fa-times text-danger-lter\' : \'fa-check text-success\' }} text"></i>'
+            }, {
+                displayName:  'Function',
+                cellTemplate: '<button ng-click="tree.addFunction(node)" class="btn btn-default btn-sm">Added Controller!</button>'
+            }, {
+                displayName:  'Remove',
+                cellTemplate: '<button ng-click="tree.remove_node(node)" class="btn btn-default btn-sm">Remove</button>'
+            }];
+        // DataDemo.getDatas() can see in 'Custom Option' -> Tab 'Data Demo'
+        $scope.tree_data = DataDemo.getBigData({
+            'DemographicId': 1,
+            'ParentId':      null,
+            'Name':          'United States of America',
+            'Description':   'United States of America',
+            'Area':          9826675,
+            'Population':    318212000,
+            'TimeZone':      'UTC -5 to -10'
+        }, 500, 7, null, 'DemographicId', 'ParentId');
+
+
 
         $scope.getperm = function () {
             var msg = {
@@ -181,7 +226,6 @@
                 },
             });
     }
-
 })();
 /**
  * Created by asus iran on 10/17/2016.
