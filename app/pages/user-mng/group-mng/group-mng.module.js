539f02f68bfe6070e7beb8044fae02e413915d89
akane0151 <akane0151@yahoo.com>
2016-10-22 15:32:26 +0330

    تفضیلی

--- app/pages/user-mng/group-mng/group-mng.module.js
+++ app/pages/user-mng/group-mng/group-mng.module.js
@@ -181,15 +181,7 @@
                 cellTemplate: '<button ng-click="tree.remove_node(node)" class="btn btn-default btn-sm">Remove</button>'
             }];
         // DataDemo.getDatas() can see in 'Custom Option' -> Tab 'Data Demo'
-        $scope.tree_data = DataDemo.getBigData({
-            'DemographicId': 1,
-            'ParentId':      null,
-            'Name':          'United States of America',
-            'Description':   'United States of America',
-            'Area':          9826675,
-            'Population':    318212000,
-            'TimeZone':      'UTC -5 to -10'
-        }, 500, 7, null, 'DemographicId', 'ParentId');
+        $scope.tree_data = [{"id":"0","parent":"#","text":"","state":{"opened":true}},{"id":"14","parent":"0","text":"گروه تست","state":{"opened":true}},{"id":"20","parent":"0","text":"گروه تست 2","state":{"opened":true}},{"id":"23","parent":"14","text":"معین 3","state":{"opened":true}},{"id":"31","parent":"14","text":"معین 11","state":{"opened":true}},{"id":"34","parent":"20","text":"معین تفضیلی 1","state":{"opened":true}},{"id":"35","parent":"20","text":"گروه تستی 33","state":{"opened":true}},{"id":"37","parent":"20","text":"گروه تستی 66","state":{"opened":true}},{"id":"39","parent":"0","text":"asdasd","state":{"opened":true}},{"id":"42","parent":"35","text":"نتهعان","state":{"opened":true}},{"id":"43","parent":"35","text":"alibaba","state":{"opened":true}},{"id":"44","parent":"35","text":"ali","state":{"opened":true}}];
 
 
 
