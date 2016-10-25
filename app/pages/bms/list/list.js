98b62ead2273eae81694c49405e18edfe51939f2
akane0151 <akane0151@yahoo.com>
2016-10-25 14:00:26 +0330

    تفضیلی

--- app/pages/bms/list/list.js
+++ app/pages/bms/list/list.js
@@ -23,15 +23,11 @@
         };
     });
     list.controller('bms2', function ($scope, zdsSocket, toastr, $uibModal) {
-        var tree = [];
-
-        tree.push({'id': 1, 'parent': '#', 'text': "ساختمان ها", 'state': {'opened': true}});
-        tree.push({'id': 2, 'parent': 1, 'text': "طبقه اول", 'state': {'opened': true}});
-        tree.push({'id': 3, 'parent': 1, 'text': "ورودی", 'state': {'opened': true}});
-        tree.push({'id': 4, 'parent': 1, 'text': "راه رو", 'state': {'opened': true}});
-        tree.push({'id': 5, 'parent': 2, 'text': "اتاق اول", 'state': {'opened': true}});
-        tree.push({'id': 6, 'parent': 2, 'text': "اتاق دو", 'state': {'opened': true}});
-        tree.push({'id': 7, 'parent': 4, 'text': "پذیرش", 'state': {'opened': true}});
+        $scope.ignoreChanges = false;
+        //var newId = 0;
+        $scope.ignoreChanges = false;
+        $scope.newNode = {};
+        $scope.activeId = 0;
         $scope.basicConfig = {
 
             core: {
@@ -52,12 +48,27 @@
             'version': 1
         };
 
+
+        var tree = [];
+
+        tree.push({'id': 1, 'parent': '#', 'text': "ساختمان ها", 'state': {'opened': true}});
+        tree.push({'id': 2, 'parent': 1, 'text': "طبقه اول", 'state': {'opened': true}});
+        tree.push({'id': 3, 'parent': 1, 'text': "ورودی", 'state': {'opened': true}});
+        tree.push({'id': 4, 'parent': 1, 'text': "راه رو", 'state': {'opened': true}});
+        tree.push({'id': 5, 'parent': 2, 'text': "اتاق اول", 'state': {'opened': true}});
+        tree.push({'id': 6, 'parent': 2, 'text': "اتاق دو", 'state': {'opened': true}});
+        tree.push({'id': 7, 'parent': 4, 'text': "پذیرش", 'state': {'opened': true}});
+
         console.log(JSON.stringify(tree));
         $scope.bmstree = tree;
-        $scope.ignoreChanges = true;
-        //newId = 0;
-        $scope.treeData = $scope.bmstree;
-        $scope.basicConfig.version++;
+
+        $scope.refresh = function () {
+            $scope.ignoreChanges = true;
+            //newId = 0;
+            $scope.treeData = $scope.bmstree;
+            $scope.basicConfig.version++;
+        };
+        $scope.refresh();
     });
     /** @ngInject */
     function routeConfig($stateProvider) {
